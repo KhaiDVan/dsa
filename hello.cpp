2a4d7990c1142dd8ac40691e776d09e92eafbbc9
KhaiDVan <dinhvankhai12321@gmail.com>
2025-05-31 23:33:34 +0700

    cap nhap

--- hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Thêm thư viện iostream để sử dụng std::cout
-
-int main() {
-    std::cout << "Hello" << std::endl; // In ra "Hello" và xuống dòng
-    return 0; // Trả về 0 để báo hiệu chương trình kết thúc thành công
-}
